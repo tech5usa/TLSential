bcd299996f4adcc090353a1adc55860144964bee
DMXorcist <dmxorcist@gmail.com>
2020-04-30 21:16:46 -0700

    Fix hilarious mistake that causes the path / to never be served

--- main.go
+++ main.go
@@ -72,8 +72,8 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-			next.ServeHTTP(w, r)
 		}
+		next.ServeHTTP(w, r)
 	})
 }
 

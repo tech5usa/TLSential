1bf22204c98aaf79a7e061d7ae668a314c8bb3c1
DMXorcist <dmxorcist@gmail.com>
2020-05-02 22:44:50 -0700

    add -k to auth.sh so curl ignores self signed certificates

--- auth.sh
+++ auth.sh
@@ -6,6 +6,6 @@ HOST=$3
 
 BASIC=$(echo -n "$ID:$PASS" | base64)
  
-JWT=$(curl -s -X POST $HOST/api/authenticate -H "Authorization: Basic $BASIC")
+JWT=$(curl -s -k -X POST $HOST/api/authenticate -H "Authorization: Basic $BASIC")
 
 echo $JWT

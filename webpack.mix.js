1045624acc84ed777e3e90119f8516917d410417
Korvin Szanto <me@kor.vin>
2020-05-07 18:13:48 -0700

    Implement webpack compilation for scss with laravel mix

---
+++ webpack.mix.js
@@ -0,0 +1,4 @@
+let mix = require('laravel-mix');
+
+// See https://github.com/JeffreyWay/laravel-mix/blob/master/setup/webpack.mix.js for other mix options
+mix.sass('resources/scss/site.scss', 'static/css/site.css');
